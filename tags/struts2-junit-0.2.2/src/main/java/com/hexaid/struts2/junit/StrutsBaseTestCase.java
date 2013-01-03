2a1fb209658df78295afe963c44773f3b03e0502
belingueres@gmail.com <belingueres@gmail.com@e46f7ced-13ff-d188-2c5a-a1efd1813d0d>
2013-01-03 19:30:43 +0000

    [maven-release-plugin]  copy for tag struts2-junit-0.2.2

---
+++ tags/struts2-junit-0.2.2/src/main/java/com/hexaid/struts2/junit/StrutsBaseTestCase.java
@@ -0,0 +1,206 @@
+package com.hexaid.struts2.junit;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.StrutsJUnit4TestCase;
+import org.apache.struts2.config.StrutsXmlConfigurationProvider;
+import org.apache.struts2.dispatcher.Dispatcher;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.mock.web.MockServletContext;
+
+import com.opensymphony.xwork2.ActionProxy;
+
+/**
+ * @author Gabriel Belingueres
+ * @version 0.2.2
+ * @since 0.1
+ */
+public class StrutsBaseTestCase extends StrutsJUnit4TestCase {
+	
+	protected MockServletContext servletContext;
+	protected MockHttpServletRequest request;
+	protected MockHttpSession session;
+	protected MockHttpServletResponse response;
+	protected ActionProxy actionProxy;
+	
+	@Rule public TestName testName = new TestName();
+	
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+		
+		servletContext = new MockServletContext();
+		request = new MockHttpServletRequest();
+		session = new MockHttpSession();
+		request.setSession(session);
+		response = new MockHttpServletResponse();
+		ServletActionContext.setRequest(request);
+		ServletActionContext.setResponse(response);
+		ServletActionContext.setServletContext(servletContext);
+		
+		final DefaultConfiguration config = new DefaultConfiguration(this); 
+
+		setUpResolvingAnnotations(config);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		super.tearDown();
+		response = null;
+		session = null;
+		request.setSession(null);
+		request = null;
+		servletContext = null;
+		actionProxy = null;
+	}
+
+	/**
+	 * Create an ActionProxy, which is used to call an action through the entire interceptor stack.
+	 * NOTE: Before creating ActionProxies, you must loadConfiguration();
+	 * @param namespace the namespace of the action to execute (it may be null).
+	 * @param actionName the action name to execute.
+	 * @param methodName the method inside the action to execute (it may be null).
+	 * @return the newly created ActionProxy. 
+	 */
+	protected ActionProxy createActionProxy(final String namespace,
+			final String actionName, final String methodName) {
+		final Dispatcher dispatcher = Dispatcher.getInstance();
+		final Map<String, Object> contextMap = dispatcher.createContextMap(
+				request, response, null, servletContext);
+
+		actionProxy = 
+				this.actionProxyFactory.createActionProxy(namespace, actionName, methodName, contextMap);
+		return actionProxy;
+	}
+
+	/**
+	 * Loads the XML configuration providers given by the strutsFilenames array.
+	 * @param strutsFilenames the XML configuration files names used for this test.
+	 */
+	protected void loadConfiguration(final String[] strutsFilenames) {
+		final List<StrutsXmlConfigurationProvider> providers = new ArrayList<StrutsXmlConfigurationProvider>();
+		
+		final StrutsXmlConfigurationProvider providerStd = 
+				new StrutsXmlConfigurationProvider("struts-default.xml", true, servletContext);
+		
+		providers.add(providerStd);
+
+		for(final String filename : strutsFilenames) {
+			final StrutsXmlConfigurationProvider provider = new StrutsXmlConfigurationProvider(filename, true, servletContext);
+			provider.init(configuration);
+			
+			providers.add(provider);
+		}
+
+		final StrutsXmlConfigurationProvider[] providersArray = 
+				providers.toArray(new StrutsXmlConfigurationProvider[providers.size()]);
+		loadConfigurationProviders(providersArray);
+	}
+
+	protected void setUpResolvingAnnotations(final AbstractStrutsTestConfiguration config) throws NoSuchMethodException {
+		// type level annotation
+		Config annotation = this.getClass().getAnnotation(Config.class);
+		if (annotation != null) {
+			// initialize variables with type level values
+			config.updateIfNotEmpty(annotation);
+		}
+
+		// get method level annotation
+		annotation = this.getClass().getMethod(testName.getMethodName()).getAnnotation(Config.class);
+		if (annotation != null) {
+			// update variables if something is configured at method level
+			config.updateIfNotEmpty(annotation);
+		}
+		
+		config.setUpFixture();
+	}
+	
+	static abstract class AbstractStrutsTestConfiguration {
+		protected StrutsBaseTestCase testObject;
+		
+		protected String[] configFile = {};
+		protected String namespace = "";
+		protected String actionName = "";
+		protected String methodName = "";
+		
+		public AbstractStrutsTestConfiguration(final StrutsBaseTestCase testObject) {
+			this.testObject = testObject;
+		}
+		
+		public abstract void loadConfiguration(String[] configFile);
+		public abstract ActionProxy createActionProxy(String namespace, String actionName, String methodName);
+
+		public void setUpFixture() {
+			// prepare parameters
+			//
+			if (configFile.length == 0) {
+				// none specified
+				configFile = new String[] { "struts.xml" };
+			}
+				
+			if (actionName.isEmpty()) {
+				// defaults to the method name
+				actionName = testObject.testName.getMethodName();
+			}
+
+			if (namespace.isEmpty()) {
+				// defaults to no namespace
+				namespace = null;
+			}
+			
+			if (methodName.isEmpty()) {
+				// defaults to no method specified
+				methodName = null;
+			}
+
+			// do Struts the initialization stuff
+			//
+			loadConfiguration(configFile);
+			createActionProxy(namespace, actionName, methodName);
+		}
+
+		public void updateIfNotEmpty(final Config annotation) {
+			configFile = updateArrayIfNotEmpty(configFile, annotation.file());
+			namespace = updateIfNotEmpty(namespace, annotation.namespace());
+			actionName = updateIfNotEmpty(actionName, annotation.actionName());
+			methodName = updateIfNotEmpty(methodName, annotation.methodName());
+		}
+
+		private String[] updateArrayIfNotEmpty(final String[] current, final String[] newValue) {
+			return newValue.length == 0 ? current : newValue;
+		}
+
+		protected String updateIfNotEmpty(final String current, final String newValue) {
+			return newValue.isEmpty() ? current : newValue;
+		}
+	}
+
+	
+	static class DefaultConfiguration extends AbstractStrutsTestConfiguration {
+		
+		public DefaultConfiguration(final StrutsBaseTestCase testObject) {
+			super(testObject);
+		}
+
+		@Override
+		public void loadConfiguration(final String[] configFile) {
+			testObject.loadConfiguration(configFile);
+		}
+
+		@Override
+		public ActionProxy createActionProxy(final String namespace, final String actionName, final String methodName) {
+			return testObject.createActionProxy(namespace, actionName, methodName);
+		}
+
+	}
+
+}
---
+++ tags/struts2-junit-0.2.2/src/test/java/com/hexaid/struts2/junit/StrutsBaseTestCaseTest.java
@@ -0,0 +1,183 @@
+package com.hexaid.struts2.junit;
+
+import static org.junit.Assert.*;
+
+import java.lang.reflect.Method;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runners.model.FrameworkMethod;
+
+import com.hexaid.struts2.junit.StrutsBaseTestCase.AbstractStrutsTestConfiguration;
+import com.opensymphony.xwork2.ActionProxy;
+
+public class StrutsBaseTestCaseTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testsetUpResolvingAnnotationsWithOutAnnotations() {
+		try {
+			Test1 test = new Test1();
+			
+			Method testMethod = 
+				test.getClass().getMethod("theMethodNameIsTheActionNameTest");
+			test.testName.starting(new FrameworkMethod(testMethod));
+			
+			AbstractStrutsTestConfiguration config = new MockStrutsTestConfiguration(test);
+			test.setUpResolvingAnnotations(config);
+			
+			assertArrayEquals(new String[] {"struts.xml"}, config.configFile);
+			assertEquals(null, config.namespace);
+			assertEquals("theMethodNameIsTheActionNameTest", config.actionName);
+			assertEquals(null, config.methodName);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+	@Test
+	public void testsetUpResolvingAnnotationsWithAnnotationInTestMethod() {
+		try {
+			Test1 test = new Test1();
+			
+			Method testMethod = 
+				test.getClass().getMethod("aMethodWithAnnotationTest");
+			test.testName.starting(new FrameworkMethod(testMethod));
+			
+			AbstractStrutsTestConfiguration config = new MockStrutsTestConfiguration(test);
+			test.setUpResolvingAnnotations(config);
+			
+			assertArrayEquals(new String[] {"struts.xml"}, config.configFile);
+			assertEquals("/", config.namespace);
+			assertEquals("differentActionName", config.actionName);
+			assertEquals(null, config.methodName);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+	@Test
+	public void testsetUpResolvingAnnotationsWithAnnotationInTestClass() {
+		try {
+			Test2 test = new Test2();
+			
+			Method testMethod = 
+				test.getClass().getMethod("theMethodNameIsTheActionNameTest");
+			test.testName.starting(new FrameworkMethod(testMethod));
+			
+			AbstractStrutsTestConfiguration config = new MockStrutsTestConfiguration(test);
+			test.setUpResolvingAnnotations(config);
+			
+			assertArrayEquals(new String[] {"another-struts.xml"}, config.configFile);
+			assertEquals("/somename", config.namespace);
+			assertEquals("theMethodNameIsTheActionNameTest", config.actionName);
+			assertEquals(null, config.methodName);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+	@Test
+	public void testsetUpResolvingAnnotationsWithAnnotationInTestClassAndMethod() {
+		try {
+			Test2 test = new Test2();
+			
+			Method testMethod = 
+				test.getClass().getMethod("aMethodWithAnnotationTest");
+			test.testName.starting(new FrameworkMethod(testMethod));
+			
+			AbstractStrutsTestConfiguration config = new MockStrutsTestConfiguration(test);
+			test.setUpResolvingAnnotations(config);
+			
+			assertArrayEquals(new String[] {"another-struts.xml"}, config.configFile);
+			assertEquals("/", config.namespace);
+			assertEquals("differentActionName", config.actionName);
+			assertEquals("show", config.methodName);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+	@Test
+	public void testsetUpResolvingAnnotationsWithTwoFiles() {
+		try {
+			Test3 test = new Test3();
+			
+			Method testMethod = 
+				test.getClass().getMethod("aMethodWithAnnotationTest");
+			test.testName.starting(new FrameworkMethod(testMethod));
+			
+			AbstractStrutsTestConfiguration config = new MockStrutsTestConfiguration(test);
+			test.setUpResolvingAnnotations(config);
+			
+			assertArrayEquals(new String[]{"struts-plugin.xml", "another-struts.xml"}, config.configFile);
+			assertEquals("/", config.namespace);
+			assertEquals("differentActionName", config.actionName);
+			assertEquals("show", config.methodName);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+	public static class Test1 extends StrutsBaseTestCase {
+		@Test
+		public void theMethodNameIsTheActionNameTest() {
+		}
+		
+		@Test
+		@Config(actionName="differentActionName", namespace="/")
+		public void aMethodWithAnnotationTest() {
+		}
+	}
+
+	@Config(file="another-struts.xml", namespace="/somename")
+	public static class Test2 extends StrutsBaseTestCase {
+		@Test
+		public void theMethodNameIsTheActionNameTest() {
+		}
+		
+		@Test
+		@Config(actionName="differentActionName", namespace="/", methodName="show")
+		public void aMethodWithAnnotationTest() {
+		}
+	}
+
+	@Config(file={"struts-plugin.xml", "another-struts.xml"}, namespace="/somename")
+	public static class Test3 extends StrutsBaseTestCase {
+		@Test
+		public void theMethodNameIsTheActionNameTest() {
+		}
+		
+		@Test
+		@Config(actionName="differentActionName", namespace="/", methodName="show")
+		public void aMethodWithAnnotationTest() {
+		}
+	}
+	
+	private static class MockStrutsTestConfiguration extends StrutsBaseTestCase.AbstractStrutsTestConfiguration {
+		public MockStrutsTestConfiguration(StrutsBaseTestCase testObject) {
+			super(testObject);
+		}
+
+		@Override
+		public void loadConfiguration(String[] configFile) {
+			// do nothing
+		}
+
+		@Override
+		public ActionProxy createActionProxy(String namespace, String actionName,
+				String methodName) {
+			// do nothing
+			return null;
+		}
+	}
+
+}
